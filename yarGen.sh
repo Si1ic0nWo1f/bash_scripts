e439d8ede06afe3446277ea7eb1fa35b6d88d252
SiliconW01f <31215734+SiliconW01f@users.noreply.github.com>
2017-08-22 22:15:09 +0100

    Update yarGen.sh

--- yarGen.sh
+++ yarGen.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Variables
+## Variables
 SAMPLES=~/Desktop/YARA_Samples
 RULE=yarGen_Rule.yar
 GIT=~/git
 VERBOSE=0
 
-# Display help and usage information
+## Display help and usage information
 if [[ ${1} = "-h" ]]
 then
         echo "usage: ./yarGen.sh [-h] [-s] [-v] [-sv]"
@@ -28,13 +28,13 @@ fi
 
 clear
 
-# Change default samples directory
+## Change default samples directory
 if [[ ${1} = "-s" ]]
 then
 	SAMPLES=${2}
 fi
 
-# Update strings database and dependencies
+## Update strings database and dependencies
 if [[ ${1} = "-u" ]]
 then
 	apt-get update
@@ -48,7 +48,7 @@ then
 	exit
 fi
 
-# Verbose - Display all output
+## Verbose - Display all output
 if [[ ${1} = "-v" ]]
 then
 	VERBOSE=1
@@ -56,7 +56,7 @@ then
 	echo ""
 fi
 
-# Change default samples directory and display all output
+## Change default samples directory and display all output
 if [[ ${1} = "-sv" ]]
 then
         SAMPLES=${2}
@@ -65,13 +65,13 @@ then
         echo ""
 fi
 
-# If git folder doesn't exist then create
+## If git folder doesn't exist then create
 if [[ ! -d ${GIT} ]]
 then
         mkdir ${GIT}
 fi
 
-# If yarGen folder doesn't exist then clone github repostitory and install dependencies
+## If yarGen folder doesn't exist then clone github repostitory and install dependencies
 if [[ ! -d ${GIT}/yarGen ]]
 then
         cd ${GIT}
@@ -84,20 +84,20 @@ then
 	apt-get install python2.7 -y
 fi
 
-# If samples folder doesn't exist then create
+## If samples folder doesn't exist then create
 if [[ ! -d ${SAMPLES} ]]
 then
         mkdir ${SAMPLES}
 fi
 
-# If database directory doesn't exist then generate
+## If database directory doesn't exist then generate
 if [[ ! -d ${GIT}/yarGen/dbs ]]
 then
         cd ${GIT}/yarGen
 	python ${GIT}/yarGen/yarGen.py --update
 fi
 
-# If previous YARA rule file exists then delete
+## If previous YARA rule file exists then delete
 if [[ -e ${SAMPLES}/${RULE} ]]
 then
 	rm ${SAMPLES}/${RULE}
@@ -108,7 +108,7 @@ then
 	clear
 fi
 
-# Main title
+## Main title
 echo -e "\e[1;92m-----------------------------------------------"
 echo -e "\e[0myarGen - YARA rule creation Script"
 echo ""
@@ -116,12 +116,12 @@ echo "Powered by https://github.com/Neo23x0/yarGen"
 echo -e "\e[1;92m-----------------------------------------------"
 echo ""
 
-# Pause to allow files to be copied into the sample folder
+## Pause to allow files to be copied into the sample folder
 echo -e "\e[1;92mCopy Malware binaries to $SAMPLES"
 read -p "Press [Enter] key to Continue..."
 echo ""
 
-# If samples folder is empty display a message and exit
+## If samples folder is empty display a message and exit
 if [ ! "$(ls -A $SAMPLES)" ]
 then
 	echo -e "\e[1;31mThe YARA Samples Folder is Empty"
@@ -129,16 +129,16 @@ then
 	exit
 fi
 
-# Input for author field
+## Input for author field
 echo -ne "\e[1;92mEnter name for the rule author field: \e[0m"
 read author
 echo ""
 
-# Message displayed while Python script runs
+## Message displayed while Python script runs
 echo "Generating YARA rule..."
 echo "This may take several minutes..."
 
-# Main Python script with STDOUT redirected to /dev/null unless VERBOSE = 1
+## Main Python script with STDOUT redirected to /dev/null unless VERBOSE = 1
 
 if [[ ${VERBOSE} != 1 ]]
 then
@@ -153,7 +153,7 @@ then
 fi
 
 echo ""
-# If YARA rule file doesn't exist then display error message and exit
+## If YARA rule file doesn't exist then display error message and exit
 if [ ! -e ${SAMPLES}/${RULE} ]
 then
 	echo -e "\e[1;31mAn error occured. The YARA rule wasn't created"
@@ -162,11 +162,11 @@ then
 	exit
 fi
 
-# Success message
+## Success message
 echo -e "\e[1;92mThe YARA rule was successfully generated"
 echo ""
 
-# Option to display the generated rule
+## Option to display the generated rule
 echo -ne "Do you want to view the rule? (y/n) \e[0m"
 read view
 
@@ -175,13 +175,13 @@ then
 	clear
 fi
 
-# If yes then display rule
+## If yes then display rule
 if echo "$view" | grep -iq "^y"
 then
 	cat ${SAMPLES}/${RULE}
 fi
 
-# Option to test the rule against the files in the sample folder
+## Option to test the rule against the files in the sample folder
 echo ""
 echo -ne "\e[1;92mDo you want to test the rule? (y/n) \e[0m"
 read TEST
@@ -191,7 +191,7 @@ then
 	clear
 fi
 
-# If yes then test rule
+## If yes then test rule
 if echo "$TEST" | grep -iq "^y"
 then
 	yara ${SAMPLES}/${RULE} ${SAMPLES} -r
