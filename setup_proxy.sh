bd8a16a3be97c677406c287b4a9210d71bec1810
SiliconW01f <31215734+SiliconW01f@users.noreply.github.com>
2017-09-25 13:23:28 +0100

    Create setup_proxy.sh

---
+++ setup_proxy.sh
@@ -0,0 +1,5 @@
+echo export http_proxy='http://10.0.100.200:8080' >> /etc/profile
+echo export https_proxy='http://10.0.100.200:8080' >> /etc/profile
+echo export ftp_proxy='http://10.0.100.200:8080' >> /etc/profile
+echo""
+echo Now add Defaults env_keep = "http_proxy ftp_proxy" to the /etc/sudoers file after Defaults env_reset
