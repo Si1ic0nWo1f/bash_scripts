c3de84da8c789c9eb951404e92c2907fb8c3f38e
SiliconW01f <31215734+SiliconW01f@users.noreply.github.com>
2017-09-26 10:37:07 +0100

    Update setup_proxy.sh

--- setup_proxy.sh
+++ setup_proxy.sh
@@ -2,4 +2,4 @@ echo export http_proxy='http://10.0.100.200:8080' >> /etc/profile
 echo export https_proxy='http://10.0.100.200:8080' >> /etc/profile
 echo export ftp_proxy='http://10.0.100.200:8080' >> /etc/profile
 echo""
-echo echo -e "Now add \e[1;92mDefaults env_keep = "http_proxy ftp_proxy" \e[0mto the \e[1;92m/etc/sudoers\e[0m file after \e[1;92mDefaults env_reset\e[0m"
+echo echo -e "Now add \e[1;91mDefaults env_keep = "http_proxy ftp_proxy" \e[0mto the \e[1;91m/etc/sudoers\e[0m file after \e[1;91mDefaults env_reset\e[0m"
