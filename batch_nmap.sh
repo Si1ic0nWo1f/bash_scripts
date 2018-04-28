2f799be46a8037cff2dc55da360863047ab669cf
SiliconW01f <31215734+SiliconW01f@users.noreply.github.com>
2018-04-28 14:40:09 +0100

    Update batch_nmap.sh

--- batch_nmap.sh
+++ batch_nmap.sh
@@ -2,13 +2,14 @@
 
 :' 
 Place this script in a folder with a file named hosts.txt containing 1 IP address per line.
-If only scanning your local broadcast domain, add the one liner from https://github.com/SiliconW01f/bash_scripts/blob/master/local_arp_scan.sh before line 35
+If only scanning your local broadcast domain, uncomment line 12
 The entire script uses relative file paths so can be located in as many places as required without editing.
 It will track port changes, maintain lists of all open, closed and filtered ports, and keep a record of scans ran in the logs.txt file
 At present the original scan outputs will only be saved once a day and subsequent scans will overwrite the previous one.
 All port information has however already been moved to the open, closed and filtered lists.
 '
 
+## arp-scan -l | awk '{ print $1 }' | strings -7 | grep -vE 'Starting|Interface:' | sort | uniq | tee hosts.txt
 
 ## Variables
 filename=hosts.txt
