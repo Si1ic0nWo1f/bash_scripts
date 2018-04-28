caaf2c8d7099d9d60445748969aa85bc9b4872ef
SiliconW01f <31215734+SiliconW01f@users.noreply.github.com>
2018-04-28 02:43:08 +0100

    Update batch_nmap.sh

--- batch_nmap.sh
+++ batch_nmap.sh
@@ -14,6 +14,25 @@ datetime()
 	date +"%Y%m%d %T"
 }
 
+clear
+
+## Main title
+echo -e "\e[1;92m--------------------"
+echo -e "\e[0m Batch NMAP scanner"
+echo -e "\e[1;92m--------------------"
+echo ""
+
+## Check for hosts.txt
+if [ ! -f "hosts.txt" ]
+then
+	echo -e "\e[1;31mThe hosts.txt file does not exist"
+	read -p "Press [Enter] key to Quit..."
+exit
+
+fi
+
+
+
 ## If results folder doesn't exist then create
 if [[ ! -d results ]]
 then
@@ -41,10 +60,155 @@ then
 fi
 
 ## Add timestamp to changes log
-echo >> ../changes.txt && echo  $(datetime) >> ../changes.txt
+echo >> ../log.txt && echo  Scan started at $(datetime) >> ../log.txt
+
+PS3="Please choose an option "
+select option in TCP UDP TCP_and_UDP
+do
+	case $option in
+		TCP)
 
 ## Loop to iterate through hosts.txt
 while read i; do
+	
+	echo -e "\e[0m" 
+		
+	## Rename .xml files to .old-xml
+	cd xml && ls $i-TCPSYN.xml 2>/dev/null| xargs -I {} sh -c 'mv $1 `basename $1 .xml`.old-xml' - {} && cd ../..
+
+	## TCP SYN scan
+	nmap -sS -sV -oN ./results/scans/$(today)-$i-TCPSYN.nmap -oX ./results/xml/$i-TCPSYN.xml $i
+
+	cd results/ips
+	mkdir $i 2>/dev/null
+
+	## Rename *.txt to *.old
+	cd $i && ls *.txt 2>/dev/null| xargs -I {} sh -c 'mv $1 `basename $1 .txt`.out' - {} && cd ../..
+
+	## Grep for open ports and create unique list
+	grep open ./scans/$(today)-$i-TCPSYN.nmap >> ./ips/$i/open-$i.out
+	sort ./ips/$i/open-$i.out | uniq > ./ips/$i/open-$i.txt
+
+	## Grep for filtered ports and create unique list
+	grep filtered ./scans/$(today)-$i-TCPSYN.nmap >> ./ips/$i/filtered-$i.out
+	sort ./ips/$i/filtered-$i.out | uniq > ./ips/$i/filtered-$i.txt
+
+	## Grep for closed ports and create unique list
+	grep closed ./scans/$(today)-$i-TCPSYN.nmap | grep -v "are closed" | grep -v "closed ports" >> ./ips/$i/closed-$i.out
+	sort ./ips/$i/closed-$i.out | uniq > ./ips/$i/closed-$i.txt
+
+	## Remove temporary .out files
+	rm ./ips/$i/*.out
+
+	## Delete 0 byte files
+	cd ips/$i && find ./ -type f -size 0 -print0 | xargs -0 rm && cd ../..
+	
+	## Compare current and previous xml can files
+	cd xml
+	ndiff $i-TCPSYN.old-xml $i-TCPSYN.xml 2>/dev/null| grep -v initiated >> ../../log.txt
+	cd ..
+
+done < ../$filename
+
+## Delete old xml files
+cd xml
+rm *.old-xml 2>/dev/null
+
+cd ../ips
+
+# Create merged lists for all hosts
+grep -rh open >> ../../open_ports.out
+grep -rh filtered >> ../../filtered_ports.out
+grep -rh closed >> ../../closed_ports.out
+
+cd ../..
+sort open_ports.out | uniq > open_ports.txt
+sort filtered_ports.out | uniq > filtered_ports.txt
+sort closed_ports.out | uniq > closed_ports.txt
+
+## Remove *.out files
+rm *.out
+
+## Delete 0 byte files
+find ./ -type f -size 0 -print0 | xargs -0 rm
+
+		break;;
+
+	UDP)
+
+## Loop to iterate through hosts.txt
+while read i; do
+
+	echo -e "\e[0m" 
+	
+	## Rename .xml files to .old-xml
+	cd xml && ls $i-UDP.xml 2>/dev/null| xargs -I {} sh -c 'mv $1 `basename $1 .xml`.old-xml' - {} && cd ../..
+
+	## UDP scan
+ 	nmap -sU -sV -oN ./results/scans/$(today)-$i-UDP.nmap -oX ./results/xml/$i-UDP.xml $i
+
+	cd results/ips
+	mkdir $i 2>/dev/null
+
+	## Rename *.txt to *.old
+	cd $i && ls *.txt 2>/dev/null| xargs -I {} sh -c 'mv $1 `basename $1 .txt`.out' - {} && cd ../..
+
+	## Grep for open ports and create unique list
+	grep open ./scans/$(today)-$i-UDP.nmap >> ./ips/$i/open-$i.out
+	sort ./ips/$i/open-$i.out | uniq > ./ips/$i/open-$i.txt
+
+	## Grep for filtered ports and create unique list
+	grep filtered ./scans/$(today)-$i-UDP.nmap >> ./ips/$i/filtered-$i.out
+	sort ./ips/$i/filtered-$i.out | uniq > ./ips/$i/filtered-$i.txt
+
+	## Grep for closed ports and create unique list
+	grep closed ./scans/$(today)-$i-UDP.nmap | grep -v "are closed" | grep -v "closed ports" >> ./ips/$i/closed-$i.out
+	sort ./ips/$i/closed-$i.out | uniq > ./ips/$i/closed-$i.txt
+
+	## Remove temporary .out files
+	rm ./ips/$i/*.out
+
+	## Delete 0 byte files
+	cd ips/$i && find ./ -type f -size 0 -print0 | xargs -0 rm && cd ../..
+	
+	## Compare current and previous xml can files
+	cd xml
+	ndiff $i-UDP.old-xml $i-UDP.xml 2>/dev/null| grep -v initiated >> ../../log.txt
+	cd ..
+
+done < ../$filename
+
+## Delete old xml files
+cd xml
+rm *.old-xml 2>/dev/null
+
+cd ../ips
+
+# Create merged lists for all hosts
+grep -rh open >> ../../open_ports.out
+grep -rh filtered >> ../../filtered_ports.out
+grep -rh closed >> ../../closed_ports.out
+
+cd ../..
+sort open_ports.out | uniq > open_ports.txt
+sort filtered_ports.out | uniq > filtered_ports.txt
+sort closed_ports.out | uniq > closed_ports.txt
+
+## Remove *.out files
+rm *.out
+
+## Delete 0 byte files
+find ./ -type f -size 0 -print0 | xargs -0 rm
+
+		break;;
+
+	TCP_and_UDP)
+
+## Loop to iterate through hosts.txt
+while read i; do
+
+	echo -e "\e[0m" 
+
 	## Rename .xml files to .old-xml
 	cd xml && ls $i-TCPSYN.xml 2>/dev/null| xargs -I {} sh -c 'mv $1 `basename $1 .xml`.old-xml' - {} 
 	ls $i-UDP.xml 2>/dev/null| xargs -I {} sh -c 'mv $1 `basename $1 .xml`.old-xml' - {} && cd ../..
@@ -84,8 +248,8 @@ while read i; do
 	
 	## Compare current and previous xml can files
 	cd xml
-	ndiff $i-TCPSYN.old-xml $i-TCPSYN.xml 2>/dev/null| grep -v initiated >> ../../changes.txt
-	ndiff $i-UDP.old-xml $i-UDP.xml 2>/dev/null| grep -v initiated >> ../../changes.txt
+	ndiff $i-TCPSYN.old-xml $i-TCPSYN.xml 2>/dev/null| grep -v initiated >> ../../log.txt
+	ndiff $i-UDP.old-xml $i-UDP.xml 2>/dev/null| grep -v initiated >> ../../log.txt
 	cd ..
 
 done < ../$filename
@@ -111,3 +275,8 @@ rm *.out
 
 ## Delete 0 byte files
 find ./ -type f -size 0 -print0 | xargs -0 rm
+
+		break;;
+
+	esac
+done
